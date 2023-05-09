454cf393baf84f05a10c013281dad4b6c5e25c2b
andrescarlos-gm <andrescarlos2211@gmail.com>
2023-05-09 18:02:18 -0400

    animaciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Carousel.Js
+
 const slidesContainer = document.querySelector(".slides-container");
 const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
 const prevButton = document.querySelector(".prev");
@@ -10,3 +12,20 @@ nextButton.addEventListener("click", () => {
 prevButton.addEventListener("click", () => {
 	slidesContainer.scrollLeft -= slideWidth * 2;
 });
+
+// Cerrar menu hamburguesa al clickear en un elemento
+
+
+const menuItems = document.querySelectorAll('.menu li a');
+const toggler = document.querySelector('.toggler');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+	toggler.checked = false;
+  });
+});
+
+
+// Inicializacion de animate on scroll (AOS)
+
+AOS.init();
