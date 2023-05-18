0889227869c905134fea07fd8430f14c40b9bc52
andrescarlos-gm <andrescarlos2211@gmail.com>
2023-05-18 17:13:44 -0400

    movimiento carrusel

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // Carousel.Js
-
 const slidesContainer = document.querySelector(".slides-container");
 const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
 const prevButton = document.querySelector(".prev");
@@ -13,8 +12,38 @@ prevButton.addEventListener("click", () => {
 	slidesContainer.scrollLeft -= slideWidth * 2;
 });
 
-// Cerrar menu hamburguesa al clickear en un elemento
+// Función para simular el clic en el botón next
+function clickNextButton() {
+	nextButton.click();
+}
 
+// Variable para almacenar el temporizador
+let timer;
+
+// Crear un Intersection Observer para observar el carrusel
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    // Si el carrusel es visible en el viewport
+    if (entry.isIntersecting) {
+      // Iniciar el temporizador después de 4 segundos
+      timer = setTimeout(() => {
+        clickNextButton();
+        // Detener el temporizador después de mover el carrusel
+        clearTimeout(timer);
+      }, 3000); // 4 segundos
+      // Dejar de observar el carrusel después de moverlo
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+// Observar el carrusel
+observer.observe(slidesContainer);
+
+
+  
+
+// Cerrar menu hamburguesa al clickear en un elemento
 
 const menuItems = document.querySelectorAll('.menu li a');
 const toggler = document.querySelector('.toggler');
@@ -25,10 +54,5 @@ menuItems.forEach(item => {
   });
 });
 
-
 // Inicializacion de animate on scroll (AOS)
-
 AOS.init();
-
-
-
